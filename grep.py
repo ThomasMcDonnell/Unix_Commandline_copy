29f7bcc0d70ad37b74ef45ff48edf1a355f28d1c
Thomas <mcdonnellthomas89@gmail.com>
2018-08-28 18:50:13 +0100

    regex search and refactor

--- grep.py
+++ grep.py
@@ -7,6 +7,8 @@ parser = argparse.ArgumentParser(description='Rough implementation of unix\
 parser.add_argument('file', metavar='F', type=str, nargs=1)
 parser.add_argument('-w', '--word', type=str, nargs=1, help='Find all\
                     instances of given word in file.')
+parser.add_argument('-x', '--expression', type=str, nargs=1, help='Find all\
+                    instances of given expression in file.')
 args = parser.parse_args()
 
 # colors for termional output
@@ -24,17 +26,20 @@ class bcolors:
 def find_string(_file, string):
     with open(_file) as f:
         for line in f.readlines():
-            index = 0
-            while index < len(line):
-                index = line.find(string, index)
-                if index == -1:
-                    break
+            if line.find(string) >= 0:
                 print(bcolors.OKBLUE + f"\t{line}" + bcolors.ENDC, end="")
-                index += len(string)
 
 # regex text search 
-def match_string(_file, string):
-    pass
+def match_string(_file, expression):
+    regex = re.compile(expression)
+    with open(_file) as f:
+        for line in f.readlines():
+            if regex.search(line):
+                print(bcolors.OKBLUE + f"\t{line}" + bcolors.ENDC, end="")
+
 
 if __name__ == "__main__":
-
+    if args.word:
+        find_string(args.file[0], args.word[0])
+    elif args.expression:
+        match_string(args.file[0], args.expression[0])
